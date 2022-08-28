26ad4084db6a534af5676c1ae860a37e42c8f7fb
Urban Vidovič <urbanfoundit@gmail.com>
2022-08-28 03:06:46 +0200

    Updates

---
+++ precache-manifest.93f7d4b027e2631c36c377c6490cd6d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c7fe00984827c933ab",
+    "url": "/static/css/main.a90b88ef.chunk.css"
+  },
+  {
+    "revision": "35c7fe00984827c933ab",
+    "url": "/static/js/main.d1c759cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "455946ab02f0432e964aa28b965fb92c",
+    "url": "/index.html"
+  }
+];
